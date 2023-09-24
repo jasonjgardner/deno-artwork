1855d31f7f36f296187941de85b99e1bc8d72caf
Jason Gardner <im@jasongardner.co>
2023-09-24 10:18:09 -0500

    upd: Twind presets / imports
    - Added several twind presets
    - Updated deno.json import map

--- twind.config.ts
+++ twind.config.ts
@@ -1,4 +1,10 @@
-import { Options } from "$fresh/plugins/twind.ts";
+import presetTypography from "@twind/preset-typography";
+import presetForms from "@twind/preset-tailwind-forms";
+import presetTailwind from "@twind/preset-tailwind";
+import presetExt from "@twind/preset-ext";
+import presetLineClamp from "@twind/preset-line-clamp";
+import presetAutoprefix from "@twind/preset-autoprefix";
+import type { Options } from "$fresh/plugins/twind.ts";
 
 export default {
   selfURL: import.meta.url,
@@ -7,4 +13,12 @@ export default {
       sans: ["Inter", "Helvetica", "sans-serif"],
     },
   },
+  presets: [
+    presetAutoprefix(),
+    presetTailwind(),
+    presetTypography(),
+    presetLineClamp(),
+    presetForms(),
+    presetExt(),
+  ],
 } as Options;
