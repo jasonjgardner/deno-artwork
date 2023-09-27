08cb1a4fe0d8e0b9b57fe5f5ebf05aef3372480c
Jason Gardner <im@jasongardner.co>
2023-09-27 18:00:33 -0500

    fix: Uploaded missing icon

--- components/icon/mod.ts
+++ components/icon/mod.ts
@@ -1,4 +1,6 @@
 export { default as BrowserIcon } from "$icon/browser.tsx";
+export { default as ExternalLinkIcon } from "$icon/external-link.tsx";
 export { default as GitHubIcon } from "$icon/brand-github.tsx";
 export { default as InstagramIcon } from "$icon/brand-instagram.tsx";
+export { default as TrashIcon } from "$icon/trash.tsx";
 export { default as XIcon } from "📦/icon/XIcon.tsx";
