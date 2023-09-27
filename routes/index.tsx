a40ff927bc3a98f5ab09b2558d1364c7ee80fa5d
Jason Gardner <im@jasongardner.co>
2023-09-27 12:48:53 -0500

    upd: Use gallery island

--- routes/index.tsx
+++ routes/index.tsx
@@ -1,27 +1,34 @@
 import { Handlers, PageProps } from "$fresh/server.ts";
-import type { Artist, Artwork, ArtworkEntry, Reactions } from "🛠️/types.ts";
-import Item from "📦/Item.tsx";
-import { getArtwork, sortByReactionCount } from "🛠️/db.ts";
+import type { Artwork, ArtworkEntry, GitHubUser } from "🛠️/types.ts";
+import Gallery from "🏝️/Gallery.tsx";
+import { getArtwork, getArtworkEntries, sortByReactionCount } from "🛠️/db.ts";
+import { getAuthenticatedUser } from "🛠️/github.ts";
 
 interface HomeProps {
   artworks: ArtworkEntry[];
+  user: GitHubUser | null;
 }
 
 export const handler: Handlers<HomeProps> = {
   async GET(req, ctx) {
+    const user = await getAuthenticatedUser(req);
     const url = new URL(req.url);
     const { searchParams } = url;
     const { sort } = Object.fromEntries(searchParams.entries());
 
-    const artworks = await getArtwork() as Artwork[];
+    const res = {
+      user,
+      artworks: [] as ArtworkEntry[],
+    };
 
-    if (sort === "popularity") {
-      return ctx.render({ artworks: await sortByReactionCount(artworks) });
+    if (!sort) {
+      res.artworks = await getArtworkEntries();
+      return ctx.render(res);
     }
 
-    return ctx.render({
-      artworks: artworks.map((artwork) => ({ artwork, reactions: [] })),
-    });
+    res.artworks = await sortByReactionCount(await getArtwork() as Artwork[]);
+
+    return ctx.render(res);
   },
 };
 
@@ -49,12 +56,7 @@ export default function Home({ data }: PageProps<HomeProps | null>) {
           </a>
         </h2>
         <div class="gap-6 grid md:grid-cols-2 2xl:grid-cols-4 place-items-center mt-6">
-          {data.artworks.map((entry, idx) => (
-            <Item
-              key={`${entry.artwork.id}${idx}`}
-              artwork={entry.artwork}
-            />
-          ))}
+          <Gallery artworks={data.artworks} user={data.user} />
         </div>
       </div>
     </main>
